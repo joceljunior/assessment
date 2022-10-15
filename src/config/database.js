c4e35febce158bbc53bdda4c96327814d69293af
jocel.silva <jocel.junior.silva@gmail.com>
2022-10-15 18:26:52 -0300

    fix config database.

--- src/config/database.js
+++ src/config/database.js
@@ -1,10 +1,14 @@
 module.exports  =  {
     dialect: 'mysql',
-    host: 'us-cdbr-east-06.cleardb.net' || 'localhost',
+    host: 'us-cdbr-east-06.cleardb.net',
+    // host: 'localhost',
     port: 3306,
-    username: 'b8a3fb36eba90b' || 'root',
-    password: '8e6e8c6b' || 'Chinacus@17',
-    database: 'heroku_fcebc19a9678ceb' || 'assessment',
+    username: 'b8a3fb36eba90b',
+    // username: 'root',
+    password: '8e6e8c6b',
+    // password: 'Chinacus@17',
+    database: 'heroku_fcebc19a9678ceb',
+    // database: 'assessment',
     define: {
         freezeTableName: true,
         timestamps: true,
