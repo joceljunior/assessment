e2fbbcdaccbd4d6541361887fa0975ceabaa693d
jocel.silva <jocel.junior.silva@gmail.com>
2022-10-15 17:44:04 -0300

    test deploy

--- src/routes/customerRoutes.js
+++ src/routes/customerRoutes.js
@@ -8,6 +8,11 @@ routes.get('/getCustomer', Controller.getCustomer);
 routes.get('/getCustomerUrl/:url', Controller.getCustomerUrl);
 routes.post('/postCustomer', Controller.postCustomer);
 routes.put('/updateCustomer', Controller.updateCustomer);
+routes.get('/deploy', (req, res) => {
+    res.json({
+        message: "Ola"
+    })
+});
 
 
 module.exports = routes;
