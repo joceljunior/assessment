aef684461eb577d3592b4a9f56ef32dc21093ef9
jocel.silva <jocel.junior.silva@gmail.com>
2022-11-11 19:03:22 -0300

    fix[server] disable cors.

--- src/server.js
+++ src/server.js
@@ -14,6 +14,13 @@ app.use(customerRoutes);
 app.use(sectorRoutes);
 app.use(questionRoutes);
 app.use(evaluationRoutes);
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
 
 
 app.listen(port, () => {
