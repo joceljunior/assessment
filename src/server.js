71d1fe4b92c7d3399e3a06a90bceb1b548f67ebb
jocel.silva <jocel.junior.silva@gmail.com>
2022-11-11 19:20:15 -0300

    cors

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const customerRoutes = require('../src/routes/customerRoutes');
 const sectorRoutes = require('../src/routes/sectorRoutes');
 const questionRoutes = require('../src/routes/questionRoutes');
@@ -10,17 +11,18 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use(cors());
 app.use(customerRoutes);
 app.use(sectorRoutes);
 app.use(questionRoutes);
 app.use(evaluationRoutes);
-app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    next();
-});
+// app.use(function (req, res, next) {
+//     res.setHeader('Access-Control-Allow-Origin', '*');
+//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+//     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+//     res.setHeader('Access-Control-Allow-Credentials', true);
+//     next();
+// });
 
 
 app.listen(port, () => {
