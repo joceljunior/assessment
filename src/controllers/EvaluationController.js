5aa319634c0b0d38b1a957076715071afaf17d17
Marcos Fraga <marcosjsfraga@gmail.com>
2023-04-11 14:45:28 -0300

    ♻️ postEvaluation update to receive options

--- src/controllers/EvaluationController.js
+++ src/controllers/EvaluationController.js
@@ -1,17 +1,39 @@
+// const connection = require('../database/index');
+// const { Sequelize, sequelize } = require('../models');
 const Evaluation = require('../models/Evaluation');
 const EvaluationOption = require('../models/EvaluationOption');
 
 class EvaluationController {
+
     async postEvaluation(req, res) {
         try {
-            const evaluations = req.body;
-
-            await Evaluation.bulkCreate(evaluations);
+            const evaluations_json = req.body;
             
-            return res.json({"message": true});
+            // const t = await sequelize.transaction({
+            //     isolationLevel: Sequelize.Transaction.READ_UNCOMMITED,
+            // });
+
+            // await connection.transaction(async function (transaction) {
+                const evaluation = await Evaluation.create({
+                    id_question: evaluations_json.id_question,
+                    id_customer: evaluations_json.id_customer,
+                    answer: evaluations_json.answer,
+                    comment: evaluations_json.comment
+                });            
+
+                for (let i in evaluations_json.options) {
+                    const evaluationOption = await EvaluationOption.create({
+                        id_question: evaluations_json.id_question,
+                        id_session: evaluations_json.id_session,
+                        id_option: evaluations_json.options[i],
+                    });            
+                }
+            // });
+            
+            return res.status(201).json({"message": true});
 
         } catch (error) {
-            return res.status(400);
+            return res.status(500).json({"message": error.message});
         }
     }
 
