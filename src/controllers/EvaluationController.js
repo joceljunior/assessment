6ed8ad20dba8305d941e44ab29354a6f54fa3872
Marcos Fraga <marcosjsfraga@gmail.com>
2023-04-11 15:36:45 -0300

    ♻️ add transaction to postEvaluation

--- src/controllers/EvaluationController.js
+++ src/controllers/EvaluationController.js
@@ -1,39 +1,46 @@
-// const { Sequelize, sequelize } = require('../models');
+const { Sequelize } = require('sequelize');
+const connection = require('../database');
 const Evaluation = require('../models/Evaluation');
 const EvaluationOption = require('../models/EvaluationOption');
 
 class EvaluationController {
 
     async postEvaluation(req, res) {
+        const transactionInstance = await connection.transaction({
+            isolationLevel: Sequelize.Transaction.READ_UNCOMMITED,
+        });
+
         try {
             const evaluations = req.body;
-            
-            // const t = await sequelize.transaction({
-            //     isolationLevel: Sequelize.Transaction.READ_UNCOMMITED,
-            // });
 
-            // await connection.transaction(async function (transaction) {
             for (let i in evaluations) {
                 await Evaluation.create({
                     id_question: evaluations[i].id_question,
                     id_customer: evaluations[i].id_customer,
                     answer: evaluations[i].answer,
                     comment: evaluations[i].comment
-                });            
+                }, {
+                    transaction: transactionInstance
+                });
 
                 for (let i1 in evaluations[i].options) {
                     await EvaluationOption.create({
                         id_question: evaluations[i].id_question,
                         id_session: evaluations[i].id_session,
                         id_option: evaluations[i].options[i1],
+                    }, {
+                        transaction: transactionInstance
                     });            
                 }
             }
-            // });
+
+            await transactionInstance.commit();
             
             return res.status(201).json({"message": true});
 
         } catch (error) {
+            await transactionInstance.rollback();
+
             return res.status(500).json({"message": error.message});
         }
     }
